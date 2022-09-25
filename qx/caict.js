9a7d109f10107469b73ad851cbf3ca9700c75654
miroxin <31504184+xperezxin@users.noreply.github.com>
2022-09-25 10:06:59 +0800

    Update caict.js

--- qx/caict.js
+++ qx/caict.js
@@ -1,6 +1,6 @@
 var obj = JSON.parse($response.body); 
 
-obj['result']['phone'] = '13608563057';
+//obj['result']['phone'] = '13608563057';
 obj['result']['time'] = '2022.09.26 11:12:13';
 obj['result']['message'] = 'UViUtoEcWKhnGk4jrXv5i6aoK41476g0NGP9TNvi87HhbfXK1VKk8uQCjOgS9hNV';
 
