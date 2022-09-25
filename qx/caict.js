e2f2c25493751a254b801321976b7547ee09aaf4
miroxin <31504184+xperezxin@users.noreply.github.com>
2022-09-25 09:49:16 +0800

    Add files via upload

--- qx/caict.js
+++ qx/caict.js
@@ -1,6 +1,8 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
+objdata['result']['phone'] = '13608563057';
+objdata['result']['time'] = '2022.09.26 11:12:13';
 objdata['result']['message'] = 'UViUtoEcWKhnGk4jrXv5i6aoK41476g0NGP9TNvi87HhbfXK1VKk8uQCjOgS9hNV';
 body = JSON.stringify(obj);
 
